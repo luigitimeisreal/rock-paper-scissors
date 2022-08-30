b66129690a86df6180ea6f3bc116a876eb9f0431
Antonio Riesco Serrano <arsr11-ua>
2022-08-30 12:13:02 +0200

    fix bug parsing string to int

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const cpuNum = Math.floor(Math.random() * 3);
 const buttons = document.querySelectorAll('button');
 const result = document.querySelector('#result');
 const cpu = document.querySelector('#cpu');
 
-function playGame(cpuNum, userNum) {
-    if (cpuNum === userNum) {
-        result.textContent = "Tie!"
-    } else if (cpuNum === 1 && userNum === 2) {
-        cpu.textContent = "Rock"
-        result.textContent = "You win!"
-    } else if (cpuNum === 1 && userNum === 3) {
-        cpu.textContent = "Rock"
-        result.textContent = "You loose!"
-    } else if (cpuNum === 2 && userNum === 1) {
-        cpu.textContent = "Paper"
-        result.textContent = "You loose!"
-    } else if (cpuNum === 2 && userNum === 3) {
-        cpu.textContent = "Paper"
-        result.textContent = "You win!"
-    } else if(cpuNum === 3 && userNum === 1) {
-        cpu.textContent = "Scissors"
-        result.textContent = "You win!"
-    } else if (cpuNum === 3 && userNum === 2) {
-        cpu.textContent = "Paper"
-        result.textContent = "You loose!"
-    }
-}
-
 for (let button of buttons) {
-    button.addEventListener('click', playGame(cpuNum, button.id));
+    button.addEventListener('click', function playGame() {
+        const cpuNum = Math.floor(Math.random() * 3) + 1;
+        console.log('Entra', cpuNum, parseInt(button.id))
+        if (cpuNum === parseInt(button.id)) {
+            result.textContent = "Tie!"
+        } else if (cpuNum === 1 && parseInt(button.id) === 2) {
+            cpu.textContent = "Rock"
+            result.textContent = "You win!"
+        } else if (cpuNum === 1 && parseInt(button.id) === 3) {
+            cpu.textContent = "Rock"
+            result.textContent = "You loose!"
+        } else if (cpuNum === 2 && parseInt(button.id) === 1) {
+            cpu.textContent = "Paper"
+            result.textContent = "You loose!"
+        } else if (cpuNum === 2 && parseInt(button.id) === 3) {
+            cpu.textContent = "Paper"
+            result.textContent = "You win!"
+        } else if(cpuNum === 3 && parseInt(button.id) === 1) {
+            cpu.textContent = "Scissors"
+            result.textContent = "You win!"
+        } else if (cpuNum === 3 && parseInt(button.id) === 2) {
+            cpu.textContent = "Paper"
+            result.textContent = "You loose!"
+        }
+    });
+    //button.addEventListener('click', cusLog);
 }
